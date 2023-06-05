11303f1227e4d99535be8051ca254c56a1ad2903
Iheanyi Oziegbe <neoconstel@gmail.com>
2023-06-05 21:02:11 +0100

    install tailwind and sass. Project prepped up

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<script>
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1 class="text-violet-500">Hello, Time to Start!</h1>
 </template>
