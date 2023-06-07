cab5e84b7ce4cff8e73173effd64b2566120ed1d
Iheanyi Oziegbe <neoconstel@gmail.com>
2023-06-07 14:06:20 +0100

    gallery overall layout set

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,10 @@
 <template>
-    <Carousel />
+    <Carousel class="mb-16" />
+    <section class="gallery">
+        <div class="grid grid-cols-5 gap-x-3 gap-y-14 px-4 py-14 bg-gray-900">
+            <div v-for="i in 20" class="bg-cyan-300 h-44"></div>
+        </div>
+    </section>
 </template>
 
 <script>
