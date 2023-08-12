1aae5b29cb5ca9e7fa4bb1839c50d17fbb2a7545
Iheanyi Oziegbe <neoconstel@gmail.com>
2023-08-12 11:35:48 +0100

    Updates

---
+++ assets/AboutView-fdd6bbcd.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c}from"./index-6d9ec082.js";const o={},s={class:"text-pink-500"};function n(r,_){return t(),c("h1",s,"About Page!")}const f=e(o,[["render",n]]);export{f as default};
