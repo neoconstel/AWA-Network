1b4fe24dae6f76874ac901502af8c47c2ecf769d
Iheanyi Oziegbe <neoconstel@gmail.com>
2023-09-09 14:11:48 +0100

    SignupModal: toggle passwords visibility with eye icon

--- src/components/SignupModal.vue
+++ src/components/SignupModal.vue
@@ -75,20 +75,36 @@
 
                             <!-- Password input -->
                             <div class="relative mb-6" data-te-input-wrapper-init>
-                                <input type="password"
+                                <input ref="password1" type="password"
                                     class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                                     id="signupFormControlInput" placeholder="Password" />
+                                <a @click.prevent="togglePassword(1)" href="">
+                                    <img v-if="!password1Reveal" class="inline h-5 absolute right-1 top-1/3"
+                                        src="/icons/iconmonstr-eye-lined.svg" alt="">
+                                </a>
+                                <a @click.prevent="togglePassword(1)" href="">
+                                    <img v-if="password1Reveal" class="inline h-5 absolute right-1 top-1/3"
+                                        src="/icons/iconmonstr-eye-off-thin.svg" alt="">
+                                </a>
                                 <label for="signupFormControlInput"
                                     class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">Password
                                 </label>
                             </div>
 
-                            <!-- confirm password input -->
+                            <!-- Confirm Password input -->
                             <div class="relative mb-6" data-te-input-wrapper-init>
-                                <input type="password"
+                                <input ref="password2" type="password"
                                     class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                                    id="signupFormControlInput" placeholder="Confirm Password" />
-                                <label for="signupFormControlInput"
+                                    id="signupFormControlInput2" placeholder="Confirm Password" />
+                                <a @click.prevent="togglePassword(2)" href="">
+                                    <img v-if="!password2Reveal" class="inline h-5 absolute right-1 top-1/3"
+                                        src="/icons/iconmonstr-eye-lined.svg" alt="">
+                                </a>
+                                <a @click.prevent="togglePassword(2)" href="">
+                                    <img v-if="password2Reveal" class="inline h-5 absolute right-1 top-1/3"
+                                        src="/icons/iconmonstr-eye-off-thin.svg" alt="">
+                                </a>
+                                <label for="signupFormControlInput2"
                                     class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">Confirm
                                     Password
                                 </label>
@@ -129,11 +145,28 @@ export default {
     },
     data() {
         return {
-
+            'password1Reveal': false,
+            'password2Reveal': false
         }
     },
     methods: {
+        togglePassword(n) {
+            let passwordField
 
+            if (n == 1) {
+                passwordField = this.$refs.password1
+                this.password1Reveal = !this.password1Reveal
+            }
+            else if (n == 2) {
+                passwordField = this.$refs.password2
+                this.password2Reveal = !this.password2Reveal
+            }
+
+            if (passwordField.getAttribute('type') == 'password')
+                passwordField.setAttribute('type', 'text')
+            else
+                passwordField.setAttribute('type', 'password')
+        }
     },
     mounted() {
     }
