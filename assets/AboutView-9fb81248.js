cad407d0c87167a9e5d78c31c6ff8a2383739e71
Iheanyi Oziegbe <neoconstel@gmail.com>
2023-09-11 14:47:27 +0100

    Updates

--- assets/AboutView-9fb81248.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c}from"./index-2edc64db.js";const o={},s={class:"text-pink-500"};function n(r,_){return t(),c("h1",s,"About Page!")}const f=e(o,[["render",n]]);export{f as default};
