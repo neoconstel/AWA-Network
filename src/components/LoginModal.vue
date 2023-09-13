76e466f3e1e1e797f651f507f96d59ac3cea9b3d
Iheanyi Oziegbe <neoconstel@gmail.com>
2023-09-13 14:07:34 +0100

    Modal and derived components use base 'X' close button

--- src/components/LoginModal.vue
+++ src/components/LoginModal.vue
@@ -3,9 +3,9 @@
     <Modal :tag="'loginModal'">
         <!-- content inside login modal -->
         <section class="">
-            <div class="relative p-10">
-                <span data-te-modal-dismiss="" class="absolute right-10"><a @click.prevent="" href=""><img
-                            src="/icons/iconmonstr-x-mark-thin.svg" alt="dismiss_button"></a></span>
+            <div>
+                <!-- <span data-te-modal-dismiss="" class="absolute right-10"><a @click.prevent="" href=""><img
+                            src="/icons/iconmonstr-x-mark-thin.svg" alt="dismiss_button"></a></span> -->
                 <!-- Left column container with background-->
                 <div class="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
                     <div class="shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12">
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -15,7 +15,15 @@ there is no confirmText and/or cancelText value passed in via binding. -->
         <div data-te-modal-dialog-ref
             class="pointer-events-none relative w-auto translate-y-[-50px] opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:max-w-[500px]">
             <div
-                class="min-[576px]:shadow-[0_0.5rem_1rem_rgba(#000, 0.15)] pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-white bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-600">
+                class="min-[576px]:shadow-[0_0.5rem_1rem_rgba(#000, 0.15)] pt-10 px-10 pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-white bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-600">
+                <button type="button"
+                    class="absolute top-10 right-10 box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
+                    data-te-modal-dismiss aria-label="Close">
+                    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd">
+                        <path
+                            d="M12 11.293l10.293-10.293.707.707-10.293 10.293 10.293 10.293-.707.707-10.293-10.293-10.293 10.293-.707-.707 10.293-10.293-10.293-10.293.707-.707 10.293 10.293z" />
+                    </svg>
+                </button>
                 <slot>
                     <div
                         class="flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
@@ -25,14 +33,14 @@ there is no confirmText and/or cancelText value passed in via binding. -->
                             {{ this.title }}
                         </h5>
                         <!--Close button-->
-                        <button type="button"
+                        <!-- <button type="button"
                             class="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
                             data-te-modal-dismiss aria-label="Close">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                                 stroke="currentColor" class="h-6 w-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                             </svg>
-                        </button>
+                        </button> -->
                     </div>
                     <!--Modal body-->
                     <div class="relative flex-auto p-4" data-te-modal-body-ref>
