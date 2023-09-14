34f1ad0e4c15597e0a7154dd06880a82565a2115
Iheanyi Oziegbe <neoconstel@gmail.com>
2023-09-14 14:21:12 +0100

    make the cancel and delete buttons for the deleteProjectModal to close modal

--- src/components/DeleteprojectModal.vue
+++ src/components/DeleteprojectModal.vue
@@ -2,12 +2,15 @@
     <!-- deleteProject modal -->
     <Modal :tag="'deleteProjectModal'">
         <!-- content inside deleteProject modal -->
-        <div class="space-y-4 pb-10">
-            <h3 v-if="this.dataStore.workType == 'projects'" class="text-center">Delete artwork: XYZ to trash?</h3>
-            <img class="w-32 aspect-square object-cover mx-auto" src="https://i.imgur.com/40qCewV.jpg" alt="">
+        <div class="space-y-4 p-10">
+            <h3 class="text-center">Delete artwork:
+                <b>"{{ this.dataStore.work.title }}"</b> to trash?
+            </h3>
+            <img class="w-32 aspect-square object-cover mx-auto" :src="this.dataStore.work.thumbnail" alt="">
             <flex class="flex justify-center gap-10">
-                <RippleButton class="w-24 inline" :buttonText="'Cancel'"></RippleButton>
-                <RippleButton class="w-24 inline bg-red-600 hover:bg-red-700 text-yellow-300" :buttonText="'Delete'" />
+                <RippleButton data-te-modal-dismiss class="w-24 inline" :buttonText="'Cancel'"></RippleButton>
+                <RippleButton data-te-modal-dismiss class="w-24 inline bg-red-600 hover:bg-red-700 text-yellow-300"
+                    :buttonText="'Delete'" />
             </flex>
         </div>
     </Modal>
