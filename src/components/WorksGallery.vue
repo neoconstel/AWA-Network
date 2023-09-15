b6b230d211081ea60f264e0b81bd97c09930f34b
Iheanyi Oziegbe <neoconstel@gmail.com>
2023-09-15 11:14:00 +0100

    WorksGallery: reduce bottom-padding

--- src/components/WorksGallery.vue
+++ src/components/WorksGallery.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="grid grid-cols-5 gap-x-4 gap-y-4 px-4 py-14 pb-44 place-content-start" ref="galleryContainer">
+    <div class="grid grid-cols-5 gap-x-4 gap-y-4 px-4 py-14 pb-16 place-content-start" ref="galleryContainer">
 
         <!-- works from multiple users (some works can have same user) -->
         <div v-for="(work, index) in works.slice(this.startIndex, this.stopIndex)" :key="index">
