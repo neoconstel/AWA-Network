c718d3fe56255ae564e096313ba4b2d49c6ebd6e
Iheanyi Oziegbe <neoconstel@gmail.com>
2023-09-28 14:10:40 +0100

    hook login form to backend

--- src/components/LoginModal.vue
+++ src/components/LoginModal.vue
@@ -15,7 +15,7 @@
 
                     <!-- Right column container -->
                     <div class="mb-12 md:mb-0 md:w-8/12 lg:w-5/8 xl:w-full">
-                        <form>
+                        <form @submit.prevent="submit">
                             <!--Sign in section-->
                             <div class="flex flex-row items-center justify-center lg:justify-start">
                                 <p class="mb-0 mr-4 text-lg">Sign in with</p>
@@ -64,7 +64,7 @@
 
                             <!-- Email input -->
                             <div class="relative mb-6" data-te-input-wrapper-init>
-                                <input type="text"
+                                <input v-model="loginID" type="text"
                                     class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                                     id="exampleFormControlInput2" placeholder="Email/Username" />
                                 <label for="exampleFormControlInput2"
@@ -74,7 +74,7 @@
 
                             <!-- Password input -->
                             <div class="relative mb-6" data-te-input-wrapper-init>
-                                <input ref="password" type="password"
+                                <input v-model="password" ref="password" type="password"
                                     class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                                     id="loginFormControlInput" placeholder="Password" />
                                 <a @click.prevent="togglePassword" href="">
@@ -116,7 +116,7 @@
 
                             <!-- Login button -->
                             <div class="text-center lg:text-left">
-                                <button type="button"
+                                <button type="submit"
                                     class="inline-block rounded bg-primary px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
                                     data-te-ripple-init data-te-ripple-color="light">
                                     Login
@@ -149,7 +149,9 @@ export default {
     },
     data() {
         return {
-            'passwordReveal': false
+            'passwordReveal': false,
+            'loginID': '',
+            'password': ''
         }
     },
     methods: {
@@ -161,6 +163,26 @@ export default {
                 passwordField.setAttribute('type', 'text')
             else
                 passwordField.setAttribute('type', 'password')
+        },
+        async submit() {
+            var myHeaders = new Headers();
+
+            var raw = JSON.stringify({
+                "username": this.loginID,
+                "password": this.password
+            });
+
+            var requestOptions = {
+                method: 'POST',
+                headers: myHeaders,
+                body: raw,
+                redirect: 'follow'
+            };
+
+            fetch("http://127.0.0.1:8000/auth/login/", requestOptions)
+                .then(response => response.text())
+                .then(result => console.log(result))
+                .catch(error => console.log('error', error));
         }
     },
     mounted() {
