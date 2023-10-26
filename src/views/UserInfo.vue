86cb91d77296f4c30d1af5cb46632a38bbbb11de
Iheanyi Oziegbe <neoconstel@gmail.com>
2023-10-26 11:04:58 +0100

    random tweaks on userInfo page

--- src/views/UserInfo.vue
+++ src/views/UserInfo.vue
@@ -1,59 +1,35 @@
 <template>
     <h1>User Info</h1>
-    <div>
-        <p>username: {{ this.username }}</p>
-        <p>userID: {{ this.userID }}</p>
-        <p>status: {{ this.status }}</p>
+    <div v-if="this.dataStore.user.id">
+        <!-- <p>username: {{ this.dataStore.user.username }}</p>
+        <p>userID: {{ this.dataStore.user.id }}</p> -->
+        <p>User: {{ this.dataStore.user }}</p>
+        <p>status: Logged in</p>
     </div>
+    <div v-else>
+        <p>status: Not logged in</p>
+    </div>
+
     <hr>
 </template>
 
 <script>
+// state management
+import { mapStores } from 'pinia'; // mapStores gives us access to the state
+import useDataStore from '@/stores/states'; // convention: use<storeID>Store
+
 export default {
     name: 'userInfo',
     data() {
         return {
-            'success': null,
-            'username': '',
-            'userID': '',
-            'status': 'anonymous'
+
         }
     },
+    computed: {
+        ...mapStores(useDataStore)
+    },
     mounted() {
-        const url = "http://127.0.0.1:8000/auth/user_info/"
 
-        const headers = {
-            'Content-Type': 'application/json'
-        }
-
-        const requestOptions = {
-            method: 'GET',
-            headers: headers,
-            credentials: 'include',
-            redirect: 'follow'
-        };
-
-        fetch(url, requestOptions)
-            .then((response) => {
-                if (response.status.toString()[0] == '2')
-                    this.success = true
-                else
-                    this.success = false
-
-                return response.json()
-            })
-            .then((data) => {
-                console.log(data)
-                if (this.success == true) {
-                    this.username = data['username']
-                    this.userID = data['userID']
-                    this.status = data['status']
-                }
-            }
-            )
-            .catch((error) => {
-                console.log('error', error)
-            })
     }
 }
 </script>
