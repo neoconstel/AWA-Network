758cc2aaca9438c4225f7972c71bdeeb0f1fa3f3
Iheanyi Oziegbe <neoconstel@gmail.com>
2023-12-01 15:06:15 +0100

    Carousel: can work fine with ANY number of images

--- src/components/CarouselComponent.vue
+++ src/components/CarouselComponent.vue
@@ -6,12 +6,11 @@
             <button type="button" data-te-target="#carouselExampleIndicators" data-te-slide-to="0" data-te-carousel-active
                 class="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
                 aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-te-target="#carouselExampleIndicators" data-te-slide-to="1"
-                class="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
-                aria-label="Slide 2"></button>
-            <button type="button" data-te-target="#carouselExampleIndicators" data-te-slide-to="2"
-                class="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
-                aria-label="Slide 3"></button>
+            <template v-if="this.images.length >= 2" v-for="(image, index) in images.slice(1)" :key="index">
+                <button type="button" data-te-target="#carouselExampleIndicators" :data-te-slide-to="index + 1"
+                    class="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+                    aria-label="Slide 2"></button>
+            </template>
         </div>
 
         <!--Carousel items-->
