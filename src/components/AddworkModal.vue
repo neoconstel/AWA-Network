3be8a35eb76a6eea362ca92021530e946bb9dff2
Iheanyi Oziegbe <neoconstel@gmail.com>
2023-12-03 13:29:56 +0100

    AddworkModal: fetch art categories from backend

--- src/components/AddworkModal.vue
+++ src/components/AddworkModal.vue
@@ -8,14 +8,13 @@
                 class="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
                 type="text" id="title" placeholder="Title" />
 
-            <div>
+            <div v-if="this.artCategories.length">
                 <input class="outline-gray-300 outline-none outline-2" type="text" list="sub-topics" id="categories"
                     placeholder="Category" />
                 <datalist id="sub-topics">
-                    <option value="Character Art"></option>
-                    <option value="Buildings"></option>
-                    <option value="Worlds"></option>
-                    <option value="Objects"></option>
+                    <template v-for="category in this.artCategories">
+                        <option :value="category.name"></option>
+                    </template>
                 </datalist>
             </div>
 
@@ -42,13 +41,20 @@ export default {
     },
     data() {
         return {
-
+            'artCategories': []
         }
     },
     methods: {
+        async getArtCategories() {
+            const url = `${import.meta.env.VITE_BACKEND_DOMAIN}/api/art-categories/`
+            const categories = await fetch(url)
+                .then(response => response.json())
 
+            this.artCategories = categories
+        }
     },
     mounted() {
+        this.getArtCategories()
     }
 }
 </script>
