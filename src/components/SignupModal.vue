2d78f4df4575772a711e10e54b6eded23c5b7916
Iheanyi Oziegbe <neoconstel@gmail.com>
2023-12-04 00:50:14 +0100

    SignUp modal: add First name and Last name fields (working at backend)

--- src/components/SignupModal.vue
+++ src/components/SignupModal.vue
@@ -8,10 +8,10 @@
                             src="/static/icons/iconmonstr-x-mark-thin.svg" alt="dismiss_button"></a></span> -->
                 <!-- Left column container with background-->
                 <div class="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between pb-10">
-                    <div class="shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12">
+                    <!-- <div class="shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12">
                         <img src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
                             class="w-full" alt="Sample image" />
-                    </div>
+                    </div> -->
 
                     <!-- Right column container -->
                     <div class="mb-12 md:mb-0 md:w-8/12 lg:w-5/8 xl:w-full">
@@ -81,6 +81,30 @@
                                 <label for="exampleFormControlInput2B"
                                     class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">Username
                                 </label>
+                            </div>
+
+                            <div class="grid grid-cols-2 gap-x-5">
+                                <!-- First name -->
+                                <div class="relative mb-6" data-te-input-wrapper-init>
+                                    <input v-model="firstName" type="text"
+                                        class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                                        id="exampleFormControlInput2BC" placeholder="First Name" />
+                                    <label for="exampleFormControlInput2BC"
+                                        class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">First
+                                        Name
+                                    </label>
+                                </div>
+
+                                <!-- Last name -->
+                                <div class="relative mb-6" data-te-input-wrapper-init>
+                                    <input v-model="lastName" type="text"
+                                        class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                                        id="exampleFormControlInput2BD" placeholder="Last Name" />
+                                    <label for="exampleFormControlInput2BD"
+                                        class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">Last
+                                        Name
+                                    </label>
+                                </div>
                             </div>
 
                             <!-- Password input -->
@@ -170,6 +194,8 @@ export default {
             'password2Reveal': false,
             'email': '',
             'username': '',
+            'firstName': '',
+            'lastName': '',
             'password1': '',
             'password2': '',
             'success': null,
@@ -202,6 +228,8 @@ export default {
             const data = JSON.stringify({
                 "email": this.email,
                 "username": this.username,
+                "first_name": this.firstName,
+                "last_name": this.lastName,
                 "password": this.password1,
                 "password2": this.password2
             });
