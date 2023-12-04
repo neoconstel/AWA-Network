c9f2182c306710fde3e16271cebfcdb4915cd0f3
Iheanyi Oziegbe <neoconstel@gmail.com>
2023-12-04 12:45:49 +0100

    user can delete their own artwork

--- src/components/DeleteprojectModal.vue
+++ src/components/DeleteprojectModal.vue
@@ -4,13 +4,13 @@
         <!-- content inside deleteProject modal -->
         <div v-if="this.work.id" class="space-y-4 p-10">
             <h3 class="text-center">Delete artwork:
-                <b>"{{ this.work.title }}"</b> to trash?
+                <b>"{{ this.work.title }}"</b> permanently?
             </h3>
             <img class="w-32 aspect-square object-cover mx-auto" :src="this.work.file_url" alt="">
             <flex class="flex justify-center gap-10">
                 <RippleButton data-te-modal-dismiss class="w-24 inline" :buttonText="'Cancel'"></RippleButton>
-                <RippleButton data-te-modal-dismiss class="w-24 inline bg-red-600 hover:bg-red-700 text-yellow-300"
-                    :buttonText="'Delete'" />
+                <RippleButton @click="deleteWork" data-te-modal-dismiss
+                    class="w-24 inline bg-red-600 hover:bg-red-700 text-yellow-300" :buttonText="'Delete'" />
             </flex>
         </div>
     </Modal>
@@ -37,7 +37,30 @@ export default {
         }
     },
     methods: {
+        deleteWork() {
+            const url = `${import.meta.env.VITE_BACKEND_DOMAIN}/api/artwork/${this.work.id}/`
 
+            const headers = {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': this.$cookies.get('csrftoken')
+            }
+
+            const requestOptions = {
+                method: 'DELETE',
+                headers: headers,
+                credentials: 'include'
+            };
+
+            fetch(url, requestOptions)
+                .then((response) => {
+                    if (response.status < 300) {
+                        alert(`Artwork deleted successfully`)
+                    }
+                    else {
+                        alert(`Failed to delete artwork`)
+                    }
+                })
+        }
     },
     mounted() {
         // add event listener for when this modal is shown
