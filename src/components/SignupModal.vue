5ed16bf99320b0f72fcd6427d1db8f5d5edb88b5
Iheanyi Oziegbe <neoconstel@gmail.com>
2023-12-05 16:33:04 +0100

    WIP: Signupmodal themeing -- email field themed for dark mode

--- src/components/SignupModal.vue
+++ src/components/SignupModal.vue
@@ -18,7 +18,7 @@
                         <form>
                             <!--Sign in section-->
                             <div class="flex flex-row items-center justify-center lg:justify-start">
-                                <p class="mb-0 mr-4 text-lg">Sign up with</p>
+                                <p class="mb-0 mr-4 text-lg text-gray-800 dark:text-gray-200">Sign up with</p>
 
                                 <!-- Facebook -->
                                 <button type="button" data-te-ripple-init data-te-ripple-color="light"
@@ -65,10 +65,10 @@
                             <!-- Email input -->
                             <div class="relative mb-6" data-te-input-wrapper-init>
                                 <input v-model="email" type="text"
-                                    class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                                    class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:outline-gray-400 dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                                     id="exampleFormControlInput2" placeholder="Email address" />
                                 <label for="exampleFormControlInput2"
-                                    class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">Email
+                                    class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200">Email
                                     address
                                 </label>
                             </div>
