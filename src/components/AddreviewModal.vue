e8a3c5b92a2eadb07f5fec0c30709ae571de610f
Iheanyi Oziegbe <neoconstel@gmail.com>
2024-07-29 17:44:00 +0100

    AddreviewModal: upload review (one/two medias -- image or video each)

--- src/components/AddreviewModal.vue
+++ src/components/AddreviewModal.vue
@@ -1,7 +1,7 @@
 <template>
     <Modal :tag="'addReviewModal'" ref="addreviewModal">
         <!-- content inside addReview modal -->
-        <form class="flex flex-col gap-5 mb-3 p-10">
+        <form class="flex flex-col gap-5 mb-3 p-10" enctype="multipart/form-data">
             <h3 class="text-gray-700 dark:text-gray-300">Upload new Review</h3>
 
             <input
@@ -28,9 +28,9 @@
                 class="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-gray-800 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-gray-800 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-gray-800 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
                 type="file" id="formFile" accept="image/*,video/*" ref="fileInput" />
 
-            <!-- <input
+            <input
                 class="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-gray-800 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-gray-800 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-gray-800 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
-                type="file" id="formFile" accept="image/*,video/*" ref="fileInput2" /> -->
+                type="file" id="formFile2" accept="image/*,video/*" ref="fileInput2" />
 
             <RippleButton @click="submit" :buttonText="'Upload'" type="button"
                 class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
@@ -70,7 +70,10 @@ export default {
             const artCategoryID = artCategory.id
             const content = this.$refs.content.value
             const tags = this.$refs.tags.value
-            const file = this.$refs.fileInput.files[0]
+            const captionMediaFile = this.$refs.fileInput.files[0]
+            let bodyMediaFile = null
+            if (this.$refs.fileInput2.files.length > 0)
+                bodyMediaFile = this.$refs.fileInput2.files[0]
 
             const url = `${import.meta.env.VITE_BACKEND_DOMAIN}/api/reviews/`
 
@@ -83,8 +86,44 @@ export default {
             formData.append('category', artCategoryID)
             formData.append('content', content)
             formData.append('tags', tags)
-            formData.append('file_type', 'image') // TODO: set this from the form
-            formData.append('file', file)
+
+            // assign the media file types based on the file extensions
+            let caption_file_type = ""
+            let body_file_type = ""
+            const imageExtensions = ['jpg', 'jpeg', 'png', 'bmp']
+            const videoExtensions = ['mp4', 'mov', 'avi']
+
+            const caption_file_extension = captionMediaFile.name.split('.').slice('-1')[0]
+            console.log(`Caption file extension: ${caption_file_extension}`)
+            if (imageExtensions.includes(caption_file_extension))
+                caption_file_type = 'image'
+            else if (videoExtensions.includes(caption_file_extension))
+                caption_file_type = 'video'
+
+            if (bodyMediaFile) {
+                const body_file_extension = bodyMediaFile.name.split('.').slice('-1')[0]
+                console.log(`Body file extension: ${body_file_extension}`)
+                if (imageExtensions.includes(body_file_extension))
+                    body_file_type = 'image'
+                else if (videoExtensions.includes(body_file_extension))
+                    body_file_type = 'video'
+                else
+                    body_file_type = null
+            }
+
+            // console.log(`Caption file type: ${caption_file_type}`)
+            // console.log(`Body file type: ${body_file_type}`)
+            // console.log(imageExtensions)
+            // console.log(caption_file_extension)
+            // console.log(imageExtensions.includes(caption_file_extension))
+
+            // append the files and file types            
+            formData.append('caption_file_type', caption_file_type)
+            formData.append('caption_file', captionMediaFile)
+
+            formData.append('body_file_type', body_file_type)
+            if (bodyMediaFile)
+                formData.append('body_file', bodyMediaFile)
 
 
             const requestOptions = {
