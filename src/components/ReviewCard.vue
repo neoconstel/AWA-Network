9282d913001ceaf422255713f982f6cb46110a87
Iheanyi Oziegbe <neoconstel@gmail.com>
2024-08-11 00:27:11 +0100

    ReviewCard: display real data

--- src/components/ReviewCard.vue
+++ src/components/ReviewCard.vue
@@ -7,9 +7,8 @@
 
         <div class="bg-gray-500 dark:bg-gray-900 flex-grow rounded-b-3xl grid px-2"
             style="grid-template-rows: 2fr 7fr 1fr;">
-            <h2 class="text-2xl my-auto">Title Goes Here</h2>
-            <p class="mt-3">This is the body text preview This is the body text preview
-                This is the body text preview This is the body text preview
+            <h2 class="text-2xl my-auto">{{ review.title }}</h2>
+            <p class="mt-3">{{ review.content.slice(0, 250) }}
             </p>
             <p class="ml-3">
                 <RouterLink class="text-gray-200 dark:text-cyan-500 hover:text-pink-700 dark:hover:text-gray-100"
