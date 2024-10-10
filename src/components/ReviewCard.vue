61dac0e6aabb0f84f7f8afd3275ad2560ce2f725
Iheanyi Oziegbe <neoconstel@gmail.com>
2024-10-10 09:09:35 +0100

    ReviewCard: remove roundness

--- src/components/ReviewCard.vue
+++ src/components/ReviewCard.vue
@@ -1,12 +1,11 @@
 <template>
-    <div class="flex flex-col rounded-full" style="aspect-ratio: 9/16;">
+    <div class="flex flex-col" style="aspect-ratio: 9/16;">
         <RouterLink class="w-full h-1/3" :to="`/review/${review.id}/`">
-            <img class="h-full w-full object-cover object-top rounded-t-3xl" :src="review.caption_media_url" alt="">
+            <img class="h-full w-full object-cover object-top" :src="review.caption_media_url" alt="">
         </RouterLink>
-        <!-- <img class="object-cover object-top h-1/3 w-full rounded-t-3xl" :src="review.caption_media_url" alt=""> -->
+        <!-- <img class="object-cover object-top h-1/3 w-full" :src="review.caption_media_url" alt=""> -->
 
-        <div class="bg-gray-500 dark:bg-gray-900 flex-grow rounded-b-3xl grid px-2"
-            style="grid-template-rows: 2fr 7fr 1fr;">
+        <div class="bg-gray-500 dark:bg-gray-900 flex-grow grid px-2" style="grid-template-rows: 2fr 7fr 1fr;">
             <h2 class="text-2xl my-auto overflow-hidden whitespace-nowrap text-ellipsis">{{ review.title }}</h2>
             <p class="mt-3">{{ review.content.slice(0, 200) + (review.content.length > 200 ? '...' : '') }}
             </p>
