4ad8df4d13e4e8228876ad0b7a7631ef23c3f16b
Iheanyi Oziegbe <neoconstel@gmail.com>
2024-10-13 00:34:18 +0100

    AddreviewModal: set rating

--- src/components/AddreviewModal.vue
+++ src/components/AddreviewModal.vue
@@ -21,12 +21,20 @@
             <textarea class="outline-gray-300 outline-none outline-2" rows="3" columns="5" placeholder="Review text"
                 ref="content"></textarea>
 
+            <p>Rating: {{ this.rating }}</p>
+
+            <input v-model="this.rating" class="w-full" type="range" id="rating-slider" min="0" max="5" step="0.5"
+                value="0" name="temp" list="values" />
+            <datalist id="values" class="flex flex-row justify-between w-full">
+                <option v-for="i in 11" :value="(i - 1) / 2" :label="(i - 1) / 2" :key="i"></option>
+            </datalist>
+
             <textarea class="outline-gray-300 outline-none outline-2" rows="3" columns="5"
                 placeholder="Tags (separated by commas)" ref="tags"></textarea>
 
             <label for="formFile1" class="text-gray-700 dark:text-gray-300 outline outline-1 p-0.5">{{
                 captionMediaLabel
-                }}</label>
+            }}</label>
 
             <input @change="onSelectCaptionMedia"
                 class="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-gray-800 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-gray-800 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-gray-800 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
@@ -34,7 +42,7 @@
 
             <label for="formFile2" class="text-gray-700 dark:text-gray-300 outline outline-1 p-0.5">{{
                 bodyMediaLabel
-                }}</label>
+            }}</label>
 
             <input @change="onSelectBodyMedia"
                 class="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-gray-800 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-gray-800 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-gray-800 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
@@ -64,7 +72,8 @@ export default {
             'captionMediaLabel': '',
             'bodyMediaLabel': '',
             'defaultCaptionMediaLabel': 'Select Caption Image*',
-            'defaultBodyMediaLabel': 'Select Body Image/Video (optional)'
+            'defaultBodyMediaLabel': 'Select Body Image/Video (optional)',
+            'rating': 0
         }
     },
     computed: {
@@ -100,6 +109,7 @@ export default {
             formData.append('category', artCategoryID)
             formData.append('content', content)
             formData.append('tags', tags)
+            formData.append('rating', this.rating)
 
             // assign the media file types based on the file extensions
             let caption_file_type = ""
