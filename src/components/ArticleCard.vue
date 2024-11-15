7c1700b7a8d1223af963697a34681e51ee470be3
Iheanyi Oziegbe <neoconstel@gmail.com>
2024-11-15 16:34:32 +0100

    ArticleCard: route to detail

--- src/components/ArticleCard.vue
+++ src/components/ArticleCard.vue
@@ -2,13 +2,14 @@
     <div v-if="article">
         <div class="grid">
             <div class="bg-pink-500">
-                <RouterLink class=""><img class="aspect-video object-cover w-full" :src="article.thumbnail_url" alt="">
+                <RouterLink class="" :to="`/article/${article.id}/`"><img class="aspect-video object-cover w-full"
+                        :src="article.thumbnail_url" alt="">
                 </RouterLink>
             </div>
             <div class="grid grid-rows-3 px-3 max-h-24 bg-yellow-500 dark:bg-yellow-700">
                 <p class="overflow-hidden whitespace-nowrap text-ellipsis">{{ article.categories.split(',').join(' ') }}
                 </p>
-                <RouterLink>
+                <RouterLink :to="`/article/${article.id}/`">
                     <h3 class="overflow-hidden whitespace-nowrap text-ellipsis">{{ article.title }}</h3>
                 </RouterLink>
                 <p class="overflow-hidden whitespace-nowrap text-ellipsis">By AWA Team</p>
