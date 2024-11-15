a9fc4b75159be71daba8782b2f6cff54c7e9d81c
Iheanyi Oziegbe <neoconstel@gmail.com>
2024-11-15 21:30:01 +0100

    ArticleCard: make MUCH more responsive

--- src/components/ArticleCard.vue
+++ src/components/ArticleCard.vue
@@ -1,19 +1,15 @@
 <template>
-    <div v-if="article">
-        <div class="grid">
-            <div class="bg-pink-500">
-                <RouterLink class="" :to="`/article/${article.id}/`"><img class="aspect-video object-cover w-full"
-                        :src="article.thumbnail_url" alt="">
-                </RouterLink>
-            </div>
-            <div class="grid grid-rows-3 px-3 max-h-24 bg-yellow-500 dark:bg-yellow-700">
-                <p class="overflow-hidden whitespace-nowrap text-ellipsis">{{ article.categories.split(',').join(' ') }}
-                </p>
-                <RouterLink :to="`/article/${article.id}/`">
-                    <h3 class="overflow-hidden whitespace-nowrap text-ellipsis">{{ article.title }}</h3>
-                </RouterLink>
-                <p class="overflow-hidden whitespace-nowrap text-ellipsis">By AWA Team</p>
-            </div>
+    <div v-if="article" class="">
+        <RouterLink class="" :to="`/article/${article.id}/`"><img class="aspect-video object-cover w-full"
+                :src="article.thumbnail_url" alt="">
+        </RouterLink>
+        <div class="grid grid-rows-3 px-3 max-h-24 bg-yellow-500 dark:bg-yellow-700">
+            <p class="overflow-hidden whitespace-nowrap text-ellipsis">{{ article.categories.split(',').join(' ') }}
+            </p>
+            <RouterLink class="overflow-hidden whitespace-nowrap text-ellipsis" :to="`/article/${article.id}/`">
+                <h3 class="overflow-hidden whitespace-nowrap text-ellipsis">{{ article.title }}</h3>
+            </RouterLink>
+            <p class="overflow-hidden whitespace-nowrap text-ellipsis">By AWA Team</p>
         </div>
     </div>
 </template>
