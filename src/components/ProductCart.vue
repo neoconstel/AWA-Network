fe2420fc1e9a26352d7b3e31a4448ab0dd68ee35
Iheanyi Oziegbe <neoconstel@gmail.com>
2024-12-30 11:35:08 +0100

    ProductCart: compute licence_data from product and display prices for each license

--- src/components/ProductCart.vue
+++ src/components/ProductCart.vue
@@ -1,8 +1,23 @@
 <template>
-    <div v-if="licenses.length" v-for="(license, index) in licenses" :key="index">
-        <h3>{{ license.name }} license</h3>
-        <p>Files:</p>
-        <p v-for="(file, nameIndex) in license.files" :key="nameIndex">{{ file }}</p>
+    <div v-if="licenses.length" v-for="(license, index) in licenses" class="border-2 border-gray-500 m-3 p-5"
+        :key="index">
+        <p class="text-lg font-bold relative">{{ license.name }} license <span class="absolute right-0">${{
+            license.price
+                }}</span></p>
+        <p class="mt-2 text-sm text-gray-500">Files:</p>
+        <button
+            class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
+            type="button" data-twe-collapse-init data-twe-ripple-init data-twe-ripple-color="light"
+            :data-twe-target="`#licenseCollapse-${index}`" aria-expanded="false"
+            :aria-controls="`licenseCollapse-${index}`">
+            Show/Hide files
+        </button>
+
+        <div class="!visible hidden" :id="`licenseCollapse-${index}`" data-twe-collapse-item>
+            <p v-for="(file, nameIndex) in license.files" class="relative" :key="nameIndex">{{ file.name }} <span
+                    class="absolute right-0">{{ file.extension }} /
+                    {{ file.size }}</span></p>
+        </div>
     </div>
 </template>
 
@@ -12,6 +27,12 @@
 import { mapStores } from 'pinia'; // mapStores gives us access to the state
 import useDataStore from '@/stores/states'; // convention: use<storeID>Store
 
+import {
+    Collapse,
+    Ripple,
+    initTWE,
+} from "tw-elements";
+
 export default {
     name: 'Cart',
     props: {
@@ -20,6 +41,13 @@ export default {
     computed: {
         ...mapStores(useDataStore),
         licenses() {
+            /** The licenses are computed from the product object. They are
+             * simply used to group the files based on each of the licenses
+             * they have.
+             * On their own, they don't have a price attribute. The price for an
+             * individual license FOR A PARTICULAR PRODUCT is taken from the
+             * product's license data.
+             */
             if (!this.product.items)
                 return []
 
@@ -41,6 +69,11 @@ export default {
                         license.files.push(item.file)
                     }
                 }
+                // get the product's license_data and get the price from there
+                let productLicense = this.product.license_data.find(
+                    pLicence => pLicence.id == license.id)
+                license.price = productLicense.price
+
                 licenses.push(license)
             }
 
@@ -56,7 +89,7 @@ export default {
     methods: {
     },
     async mounted() {
-
+        initTWE({ Collapse, Ripple });
     },
 }
 </script>
