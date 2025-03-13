f2a8738d808096b31d6a5c2503acffd5cc86aa10
Iheanyi Oziegbe <neoconstel@gmail.com>
2025-03-13 16:46:37 +0100

    - create ContestCard - Challenge page: display Contest Cards

---
+++ src/components/ContestCard.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="">
+        contest card ({{ contest.title }})
+    </div>
+</template>
+<script>
+
+
+// state management
+import { mapStores } from 'pinia'; // mapStores gives us access to the state
+import useDataStore from '@/stores/states'; // convention: use<storeID>Store
+
+export default {
+
+    name: 'ContestCard',
+    props: {
+        'contest': Object,
+    },
+    computed: {
+        ...mapStores(useDataStore),
+    },
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+    async mounted() {
+
+    },
+
+
+}
+
+</script>
--- src/views/Challenge.vue
+++ src/views/Challenge.vue
@@ -1,11 +1,70 @@
 <template>
-    <h1 class="text-pink-500">Welcome to the Challenge view!</h1>
-    <p>This page only displays if the function which calls extra routes FROM
-        the wagtail CMS was executed successfully.
-    </p>
+    <div class="page-container mx-16 mt-10 mb-24 text-gray-800 dark:text-gray-200">
+        <header>
+            <h1>AWA 3D Art Contests</h1>
+            <p class="mt-2">Test your strengths in one of our competitive art contests.</p>
+            <div class="h-1 bg-gray-500 my-5 mb-10"></div>
+        </header>
+        <main>
+            <section class="contests-grid grid grid-cols-4">
+                <template v-for="(contest, index) in contests" :key="index">
+                    <ContestCard :contest="contest" />
+                </template>
+            </section>
+        </main>
+    </div>
 </template>
 
 
 <script>
+// state management
+import { mapStores } from 'pinia'; // mapStores gives us access to the state
+import useDataStore from '@/stores/states'; // convention: use<storeID>Store
 
+import ContestCard from "@/components/ContestCard.vue";
+
+export default {
+    name: 'Challenge',
+    components: {
+        ContestCard
+    },
+    data() {
+        return {
+            "contests": []
+        }
+    },
+    computed: {
+        ...mapStores(useDataStore)
+    },
+    methods: {
+        async fetchContests() {
+            const url = `${import.meta.env.VITE_BACKEND_DOMAIN}/api/contests/`
+
+            fetch(url)
+                .then(response => response.json())
+                .then((data) => {
+                    // console.log(data)
+                    this.contests.splice(this.contests.length, 0, ...(data['results']))
+                    console.log(this.contests)
+                }
+                )
+                .catch((error) => {
+                    this.errorMessage = error
+                    console.log('error', error)
+                })
+        },
+    },
+    async mounted() {
+        console.log('Contests view mounted')
+
+        this.fetchContests()
+
+        this.$nextTick(() => {
+            setTimeout(() => {
+                window.scrollTo(0, 0);
+            }, 100)
+        })
+
+    }
+}
 </script>
