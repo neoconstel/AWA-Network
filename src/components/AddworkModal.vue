f662c0b58036c7d010b50adb5663cc77335d7e0b
Iheanyi Oziegbe <neoconstel@gmail.com>
2025-04-23 02:04:11 +0100

    ArtworkModal text themed

--- src/components/AddworkModal.vue
+++ src/components/AddworkModal.vue
@@ -5,12 +5,12 @@
             <h3 class="text-gray-700 dark:text-gray-300">Upload new Artwork</h3>
 
             <input
-                class="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-gray-800 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-gray-800 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-gray-800 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
+                class="relative text-gray-800 dark:text-gray-200 m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-gray-800 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-gray-800 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
                 type="text" id="title" placeholder="Title" ref="title" />
 
             <div v-if="this.artCategories.length">
-                <input class="outline-gray-300 outline-none outline-2" type="text" list="sub-topics" id="categories"
-                    placeholder="Category" ref="categories" />
+                <input class="text-gray-800 dark:text-gray-200  outline-gray-300 outline-none outline-2" type="text"
+                    list="sub-topics" id="categories" placeholder="Category" ref="categories" />
                 <datalist id="sub-topics">
                     <template v-for="category in this.artCategories">
                         <option :value="category.name"></option>
@@ -18,11 +18,11 @@
                 </datalist>
             </div>
 
-            <textarea class="outline-gray-300 outline-none outline-2" rows="3" columns="5"
-                placeholder="Describe the cool idea behind this stuff" ref="description"></textarea>
+            <textarea class="text-gray-800 dark:text-gray-200 outline-gray-300 outline-none outline-2" rows="3"
+                columns="5" placeholder="Describe the cool idea behind this stuff" ref="description"></textarea>
 
-            <textarea class="outline-gray-300 outline-none outline-2" rows="3" columns="5"
-                placeholder="Tags (separated by commas)" ref="tags"></textarea>
+            <textarea class="text-gray-800 dark:text-gray-200 outline-gray-300 outline-none outline-2" rows="3"
+                columns="5" placeholder="Tags (separated by commas)" ref="tags"></textarea>
 
             <input
                 class="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-gray-800 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-gray-800 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-gray-800 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
